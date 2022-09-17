f0f6848feee06a0f465eabde314723caf1839153
dylan olea <dylanolead@gmail.com>
2022-09-17 11:43:30 -0300

    añadiendo js

---
+++ js/index2.js
@@ -0,0 +1,8 @@
+let nombre = "coderhouse";
+let edad = 12;
+let curso = "javacript";
+let nombrecompleto = "dylan olea ";
+
+const profesion = "guardavida";
+
+prompt("ingresa tu nombre completo:");
