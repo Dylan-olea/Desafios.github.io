09c6ae40554b4fbc0db90b3c11accb8d65c1286b
dylan olea <dylanolead@gmail.com>
2022-10-01 12:20:07 -0300

    agregando js y proyecto final

---
+++ js/funciones.js
@@ -0,0 +1,44 @@
+/* function saludar() {
+  console.log("hola", nombre);
+}
+
+saludar(); */
+
+/* function calcular() {
+  debugger;
+  let primerNro = parseFloat(prompt("impresa el primer nro."));
+  let segundoNro = parseFloat(prompt("ingresar el segundo nro."));
+
+  if (!isNaN(primerNro) || !isNaN(segundoNro)) {
+    let resultado = primerNro + segundoNro;
+    console.log("resultado de la operacion", resultado);
+  } else {
+    alert("ingresar numero pendejo");
+  }
+}
+ */
+/* calcular();
+
+function calcular(primerNro, segundoNro) {
+  if (!isNaN(primerNro) || !isNaN(segundoNro)) {
+    let resultado = primerNro + segundoNro;
+    console.log("resultado de la operacion", resultado);
+  } else {
+    alert("ingresa numeros pendejo");
+  }
+}
+
+function pedirNro() {
+  let primerNro = parseFloat(prompt("ingresar el primer nro."));
+  let segundoNro = parseFloat(prompt("ingresa el segundo nro."));
+  calcular(primerNro, segundoNro);
+}
+ */
+
+/* function concatenarCompleto(nombre, apellido) {
+  console.log(nombre, apellido);
+} */
+
+function verProductos() {
+  let productos = "productos disponibles:";
+}
--- js/index2.js
+++ js/index2.js
@@ -1,8 +1,41 @@
-let nombre = "coderhouse";
-let edad = 12;
-let curso = "javacript";
-let nombrecompleto = "dylan olea ";
+function verProductos() {
+  let productos = "productos disponibles:/n";
+  "1) llavero /n" +
+    "2) figura de baby yoda /n" +
+    "3)  jabonera /n" +
+    "4) juguetes /n" +
+    "5) protesis";
 
-const profesion = "guardavida";
+  let respuesta = prompt(
+    "Ingresar el nro. de producto para ver el precio: /n" + productos
+  );
+  switch (parseInt(respuesta)) {
+    case 1:
+      console.log("Producto elegido 'llavero'. Su precio es de $400");
+      break;
 
-prompt("ingresa tu nombre completo:");
+    case 2:
+      console.log(
+        "Producto elegido 'figura de baby yoda'. Su precio es de $1600"
+      );
+      break;
+
+    case 3:
+      console.log("Producto elegido 'jabonera'. Su precio es de $620");
+      break;
+
+    case 4:
+      console.log("Producto elegido 'Rombo'. Su precio es de $2300");
+      break;
+
+    case 5:
+      console.log("Producto elegido 'Protesis'. Su precio es de $5000");
+      break;
+    default:
+      console.log("No tenemos ese producto aun...");
+  }
+  let continuar = confirm("volver a revisar el precio de los productos");
+  if (continuar) {
+    verProductos();
+  }
+}
